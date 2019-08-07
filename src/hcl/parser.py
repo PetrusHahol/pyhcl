e0acaed45d911fe260b1165fd5b45fac425abc04
Stan da Petrosian <stanislau.petrusiou@Pan-Net.eu>
2019-08-07 12:43:27 +0200

    Fixed list interpolation

--- src/hcl/parser.py
+++ src/hcl/parser.py
@@ -454,11 +454,13 @@ class HclParser(object):
         if isinstance(p[3], str) and p[3][0] != "\"" and p[3][0] != "$":
             p[3] = "${%s}" % p[3]
 
-        if isinstance(p[1], str) and p[1][0] == "\"":
-            p[1] = p[1][1:len(p[1]) - 1]
+        if p.stack[-1].type == 'LEFTBRACKET':
 
-        if isinstance(p[3], str) and p[3][0] == "\"":
-            p[3] = p[3][1:len(p[3]) - 1]
+            if isinstance(p[1], str) and p[1][0] == "\"":
+                p[1] = p[1][1:len(p[1]) - 1]
+
+            if isinstance(p[3], str) and p[3][0] == "\"":
+                p[3] = p[3][1:len(p[3]) - 1]
 
 
         p[0] = [p[1], p[3]]
